e991cffd084b9a8a7463487a44fc9eb68791e874
Yash Aryan <yasharyan307@outlook.com>
2022-05-20 09:54:58 +0530

    Removed Create NTT option from navbar

--- utils/components/NavbarOptions.ts
+++ utils/components/NavbarOptions.ts
@@ -7,7 +7,6 @@ const LoggedOutOptions = [
 const LoggedInOptions = [
     { title: "Home", href: "/home" },
     { title: "Dashboard", href: "/dashboard" },
-    { title: "Create NTTs", href: "/createNTT" },
     { title: "Notifications", href: "/" },
 ];
 
